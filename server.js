9ab826c12d0f47927a107c4b6699b99bd07342c4
DorukEmre <doruk.emre@outlook.com>
2022-06-10 19:36:28 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
+
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = process.env.DB_NAME
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+    
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.get('/',(request, response)=>{
+})
+
+app.post('/...', (request, response) => {
+})
+
+app.put('/...', (request, response) => {
+})
+
+app.delete('/...', (request, response) => {
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
